b035e809922689fc562016ff3efb01ea716a0a22
angelachk <acchecah@unal.edu.co>
2020-09-20 16:13:47 -0500

    make it better4

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-ver express = require('express');;
+var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/build/'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log('ExpressJS server listening on port', port);
+});
